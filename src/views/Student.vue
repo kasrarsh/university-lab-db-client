3f54347b0f5bf7dd82542fc4fd888f0fd3e59f03
kasra <kasramagers1377@gmail.com>
2021-05-12 15:52:23 +0430

    feat : add router to project

---
+++ src/views/Student.vue
@@ -0,0 +1,18 @@
+<template>
+    <HelloWorld></HelloWorld>
+</template>
+
+<script>
+    import HelloWorld from '../components/HelloWorld.vue'
+
+    export default {
+        name: "Student",
+        components: {
+            HelloWorld
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
